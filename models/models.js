9a85ee0d502d79e4981da46cdf8ea648ab7da409
jerry_laptop <jiajiewu1988@gmail.com>
2014-08-03 15:14:43 -0700

    first commit of server project

---
+++ models/models.js
@@ -0,0 +1,21 @@
+//models for crm project
+var mongo = require('mongodb');
+ 
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+ 
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('crmdb', server);
+
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'winedb' database");
+        db.collection('wines', {strict:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'wines' collection doesn't exist. Creating it with sample data...");
+                populateDB();
+            }
+        });
+    }
+});
